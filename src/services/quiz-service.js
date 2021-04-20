9b06197723e55de211a6007f74a7caa29d488dd1
jiayao0314 <li.jiayao@northeastern.edu>
2021-04-20 11:57:27 -0700

    add node server link

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -1,4 +1,4 @@
-const QUIZZES_URL = 'https://cm-node-server.herokuapp.com/quizzes';
+const QUIZZES_URL = 'https://cm-node-server.herokuapp.com/api/quizzes';
 
 const findAllQuizzes = () => {
     return fetch(QUIZZES_URL)
